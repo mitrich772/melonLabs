43d70d8f15b1b1dba91b7641ebbb5fc486c12e0c
mitrich772 <mitrichdmitrich772@gmail.com>
2024-04-21 21:45:36 +0300

    6 lab 4/6

--- lab6/lab6/lab6.cpp
+++ lab6/lab6/lab6.cpp
@@ -7,13 +7,90 @@ using namespace std;
 
 
 
-class Matrix {
+class Matrix { // Matrix(n)
+    private:
+
     int n;
-    int** dinamic_array2 = new int* [n];
+    double** dinamic_array;
+
+    public:
+    Matrix(int n = 3) {
+        this->dinamic_array = new double* [n];
+        this->n = n;
+        for (int i = 0; i < n; i++) {
+            this->dinamic_array[i] = new double[n];
+            for (int j = 0; j < n; j++){
+                cout << "Введите элемент[" << i << "][" << + j <<"]" << endl;
+                cin >> this->dinamic_array[i][j];
+            }
+        }
+
+    }
+    Matrix(unsigned int n) {
+        this->dinamic_array = new double* [n];
+        this->n = n;
+        for (int i = 0; i < n; i++) {
+            this->dinamic_array[i] = new double[n];
+            for (int j = 0; j < n; j++) {
+                this->dinamic_array[i][j] = i + j + 1;
+            }
+        }
+
+    }
+    void printElements() {
+        for (int i = 0; i < n;i++) {
+            for (int j = 0; j < n;j++) {
+                cout << dinamic_array[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    void minElement() {
+        int min_value = dinamic_array[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                min_value = dinamic_array[i][j] < min_value ? dinamic_array[i][j] : min_value;
+
+            }
+        }
+        cout << min_value << endl;
+    }
+    void maxElement() {
+        int max_value = dinamic_array[0][0];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                max_value = dinamic_array[i][j] > max_value ? dinamic_array[i][j] : max_value;
+
+            }
+        }
+        cout << max_value << endl;
+    }
+    void spur() {
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += dinamic_array[i][i];
+        }
+        cout <<"Spur:" << sum << endl;
+    }
+    ~Matrix() {
+        for (int i = 0; i < this->n; i++) {
+            delete[] dinamic_array[i];
+        }
+
+        delete[] dinamic_array;
+    } 
 
 };
 int main(){
-    cout << "Hello World!\n";
+    setlocale(LC_ALL, "RU");
+    Matrix first_matrix(2);
+    Matrix second_matrix(4u);
+    
+    first_matrix.printElements();
+    second_matrix.printElements();
+    second_matrix.maxElement();
+    second_matrix.minElement();
+    second_matrix.spur();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
