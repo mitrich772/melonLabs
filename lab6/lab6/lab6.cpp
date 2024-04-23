dcffe88f0fa7569f1e75abd40ffc3fb37849c032
mitrich772 <mitrichdmitrich772@gmail.com>
2024-04-23 20:49:16 +0300

    6 ready

--- lab6/lab6/lab6.cpp
+++ lab6/lab6/lab6.cpp
@@ -46,7 +46,7 @@ class Matrix { // Matrix(n)
         }
     }
     void minElement() {
-        int min_value = dinamic_array[0][0];
+        double min_value = dinamic_array[0][0];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 min_value = dinamic_array[i][j] < min_value ? dinamic_array[i][j] : min_value;
@@ -56,7 +56,7 @@ class Matrix { // Matrix(n)
         cout << min_value << endl;
     }
     void maxElement() {
-        int max_value = dinamic_array[0][0];
+        double max_value = dinamic_array[0][0];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 max_value = dinamic_array[i][j] > max_value ? dinamic_array[i][j] : max_value;
@@ -65,13 +65,85 @@ class Matrix { // Matrix(n)
         }
         cout << max_value << endl;
     }
-    void spur() {
-        int sum = 0;
+    double spur() {
+        double sum = 0;
         for (int i = 0; i < n; i++) {
             sum += dinamic_array[i][i];
         }
-        cout <<"Spur:" << sum << endl;
+        return sum;
     }
+    void transopanate() {
+        double t;
+        for (int i = 0; i < n; ++i){
+            for (int j = i; j < n; ++j){
+                t = dinamic_array[i][j];
+                dinamic_array[i][j] = dinamic_array[j][i];
+                dinamic_array[j][i] = t;
+            }
+        }
+    }
+    void clearMemory(double** a, int n) { //Функция освобождения памяти, выделенной под двумерный динамический массив
+        for (int i = 0; i < n; i++) {
+            delete[] a[i];
+        }
+        delete[] a;
+    }
+
+    int findDet() { //Рекурсивная функция вычисления определителя матрицы
+        if (n == 1)
+            return dinamic_array[0][0];
+        else if (n == 2)
+            return dinamic_array[0][0] * dinamic_array[1][1] - dinamic_array[0][1] * dinamic_array[1][0];
+        else {
+            double d = 0;
+            for (int k = 0; k < n; k++) {
+                double** m = new double* [n - 1];
+                for (int i = 0; i < n - 1; i++) {
+                    m[i] = new double[n - 1];
+                }
+                for (int i = 1; i < n; i++) {
+                    int t = 0;
+                    for (int j = 0; j < n; j++) {
+                        if (j == k)
+                            continue;
+                        m[i - 1][t] = dinamic_array[i][j];
+                        t++;
+                    }
+                }
+                d += pow(-1, k + 2) * dinamic_array[0][k] * findMinor(m, n - 1);
+                clearMemory(m, n - 1); //Освобождаем память, выделенную под алгебраическое дополнение
+            }
+            return d; //Возвращаем определитель матрицы
+        }
+    }
+
+    double findMinor(double** dinamic_array, int n) { // функция вычисления минора
+        if (n == 1)
+            return dinamic_array[0][0];
+        else if (n == 2)
+            return dinamic_array[0][0] * dinamic_array[1][1] - dinamic_array[0][1] * dinamic_array[1][0];
+        else {
+            double d = 0;
+            for (int k = 0; k < n; k++) {
+                double** m = new double* [n - 1];
+                for (int i = 0; i < n - 1; i++) {
+                    m[i] = new double[n - 1];
+                }
+                for (int i = 1; i < n; i++) {
+                    int t = 0;
+                    for (int j = 0; j < n; j++) {
+                        continue; //пропускаем
+                        m[i - 1][t] = dinamic_array[i][j];
+                        t++;
+                    }
+                }
+                d += pow(-1, k + 2) * dinamic_array[0][k] * findMinor(m, n - 1);
+                clearMemory(m, n - 1); //Освобождаем память, выделенную под алгебраическое дополнение
+            }
+            return d; //Возвращаем определитель матрицы
+        }
+    }
+
     ~Matrix() {
         for (int i = 0; i < this->n; i++) {
             delete[] dinamic_array[i];
@@ -83,14 +155,17 @@ class Matrix { // Matrix(n)
 };
 int main(){
     setlocale(LC_ALL, "RU");
-    Matrix first_matrix(2);
+    Matrix first_matrix(3);
     Matrix second_matrix(4u);
     
     first_matrix.printElements();
     second_matrix.printElements();
     second_matrix.maxElement();
     second_matrix.minElement();
-    second_matrix.spur();
+    cout<<second_matrix.spur()<<endl;
+    first_matrix.transopanate();
+    first_matrix.printElements();
+    cout << first_matrix.findDet() << endl;
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
